ecc96dc8efc1a9f782152099072e936984b0508c
Mathieu Marteau <marteau.mathieu@gmail.com>
2021-09-20 22:50:54 +0200

    Revert "Version 1.0 - major update"
    This reverts commit 3ddb135735b518a0c2ac630a231ffe221d36bd07.

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
 import ServerTable from "./src/components/ServerTable";
-import ServerIterator from "@/components/ServerIterator";
-
-export default {
-  ServerTable,
-  ServerIterator
-}
+export default ServerTable
